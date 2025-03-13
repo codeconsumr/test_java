2d6fdc616633490b118472368d2fafe1305dc4a0
codeconsumr <ToluOlundegun@gmail.com>
2025-03-13 15:29:45 +0000

    Files Added

---
+++ src/handbuilt/Ex_a.java
@@ -0,0 +1,18 @@
+package handbuilt;
+
+import ast.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Ex_a {
+
+    public static Program buildAST() {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(buildAST());
+    }
+}
---
+++ src/handbuilt/Ex_b.java
@@ -0,0 +1,17 @@
+package handbuilt;
+
+import ast.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex_b {
+
+    public static Program buildAST() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(buildAST());
+    }
+}
